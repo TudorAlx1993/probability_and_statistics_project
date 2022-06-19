fef657e6ba66d21ea8d271fb85233be9b293cc55
RaduADumitru <radu.dumitru@rocketmail.com>
2022-06-19 13:39:36 +0300

    Exercise 4

--- src/server/exercise_4.R
+++ src/server/exercise_4.R
@@ -2,15 +2,63 @@ Exercise4 <- function(input, output) {
   # ## Formulas:
   # P(A or B) = P(A) + P(B) - P(A and B)
   # if independent: P(A and B) = P(A) * P(B)
+  # if incompatible: P(A and B) = 0
   # P(A - B) = P(A and B) / P(B)
-  # P(B - A) = P(A and B) - P(A)
+  # P(B - A) = P(A and B) / P(A)
 
-  # ## ALGORITHM:
-  # LOOP: while true, for each of 3/4 equations, count how many variables in the equation are not null;
-  # if all are fully solved, output solution and break;
-  # if none have only one probability left to solve, break => cannot solve
-  # solve for missing probability in one of the equations
-  # repeat
+  GetInputData <- reactive({
+    req(input$exercise_4_page_input_a)
+    a <- input$exercise_4_page_input_a
+    
+    req(input$exercise_4_page_input_b)
+    b <- input$exercise_4_page_input_b
+    
+    if (input$exercise_4_page_input_interaction == 'unknown') {
+      show('exercise_4_page_input_a_b')
+      req(input$exercise_4_page_input_a_b)
+      a_and_b <- input$exercise_4_page_input_a_b
+      return(data.frame(a, b, a_and_b))
+    }
+    
+    if (input$exercise_4_page_input_interaction == 'indep') {
+      hide('exercise_4_page_input_a_b')
+      a_and_b <- a * b
+      return(data.frame(a, b, a_and_b))
+    }
+    
+    if (input$exercise_4_page_input_interaction == 'incomp') {
+      hide('exercise_4_page_input_a_b')
+      a_and_b <- 0
+      return(data.frame(a, b, a_and_b))
+    }
+  })
+  observeEvent(GetInputData(), {
+    show('exercise_4_page_action_button')
+  })
+  observeEvent(input$exercise_4_page_action_button, {
+    data <- GetInputData()
+    a <- data[,1][1]
+    b <- data[,2][1]
+    a_and_b <- data[,3][1]
+    a_or_b <- a + b - a_and_b
+    a_cond_b <- a_and_b / b
+    b_cond_a <- a_and_b / a
+    output$exercise_4_page_output_a_and_b <- renderText({
+      a_and_b
+      paste('P(A and B) =', a_and_b)
+    })
+    output$exercise_4_page_output_a_or_b <- renderText({
+      a_or_b
+      paste('P(A or B) =', a_or_b)
+    })
+    output$exercise_4_page_output_a_bar_b <- renderText({
+      a_cond_b
+      paste('P(A - B) =', a_cond_b)
+    })
+    output$exercise_4_page_output_b_bar_a <- renderText({
+      b_cond_a
+      paste('P(B - A) =', b_cond_a)
+    })
+  })
   
-  # return(NULL) 
 }
--- src/ui/exercise_4_page_content.R
+++ src/ui/exercise_4_page_content.R
@@ -3,17 +3,12 @@ Exercise4PageContent <- function() {
     useShinyjs(),
     h1("Exercise 4", align = 'center'),
     h2("Work with events", align = "center"),
-    # ## TODO: inputs:
+    # inputs:
     # P(A)
     # P(B)
-    # P(A and B)
-    # P(A or B)
-    # P(A|B)
-    # P(B|A)
+    # P(A and B) if unknown
     
     # output:
-    #   P(A)
-    # P(B)
     # P(A and B)
     # P(A or B)
     # P(A-B)
@@ -35,57 +30,35 @@ Exercise4PageContent <- function() {
       step = 0.1
     ),
     radioButtons(
-      'exercise_4_page_input_interaction', ## TODO better name?
+      'exercise_4_page_input_interaction', 
       label = 'Event interaction',
       choices = c('Unknown' = 'unknown',
                   'Independent' = 'indep',
                   'Incompatible' = 'incomp'
                   )
     ),
-    numericInput(
-      'exercise_4_page_input_a_and_b',
-      label = 'P(A and B)',
-      value=NULL,
-      min = 0,
-      max = 1,
-      step = 0.1
+    hidden(
+      numericInput(
+        'exercise_4_page_input_a_b',
+        label = 'P(A and B)',
+        value=NULL,
+        min = 0,
+        max = 1,
+        step = 0.1
+      )
     ),
-    numericInput(
-      'exercise_4_page_input_a_or_b',
-      label = 'P(A or B)',
-      value=NULL,
-      min = 0,
-      max = 1,
-      step = 0.1
+    hidden(
+      actionButton('exercise_4_page_action_button',
+                   'Run',
+                   icon = icon('play')
+      )
     ),
-    numericInput(
-      'exercise_4_page_input_a_cond_b',
-      label = 'P(A|B)',
-      value=NULL,
-      min = 0,
-      max = 1,
-      step = 0.1
-    ),
-    numericInput(
-      'exercise_4_page_input_b_cond_a',
-      label = 'P(B|A)',
-      value=NULL,
-      min = 0,
-      max = 1,
-      step = 0.1
-    ),
-    actionButton('exercise_4_page_action_button',
-                 'Run',
-                 icon = icon('play')
-                 ),
     tags$br(),
     tags$br(),
-    textOutput('exercise_4_page_output_a'),
-    textOutput('exercise_4_page_output_b'),
     textOutput('exercise_4_page_output_a_and_b'),
     textOutput('exercise_4_page_output_a_or_b'),
-    textOutput('exercise_4_page_output_a_minus_b'),
-    textOutput('exercise_4_page_output_b_minus_a'),
+    textOutput('exercise_4_page_output_a_bar_b'),
+    textOutput('exercise_4_page_output_b_bar_a'),
   )
   return (fluid.page)
 }

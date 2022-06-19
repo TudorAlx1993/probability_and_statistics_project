9bb1becf2bf865b7c1e740a77dd8497bd3f95f6c
RaduADumitru <radu.dumitru@rocketmail.com>
2022-06-19 16:47:36 +0300

    Exercise 3

--- src/server/exercise_3.R
+++ src/server/exercise_3.R
@@ -1,3 +1,88 @@
 Exercise3 <- function(input, output) {
-  # return(NULL) 
+  GetInputData <- reactive({
+    req(input$exercise_3_page_upload_values_x)
+    file.x <- input$exercise_3_page_upload_values_x
+    if (!all(file_ext(file.x$name) == 'csv')) {
+      showNotification("X values: You must choose a csv file!", type = 'error')
+      return(NULL)
+    }
+    
+    x.csv.data <- read.csv(file.x$datapath)
+    if (!ncol(x.csv.data) == 1) {
+      showNotification("X values: file must contain only 1 column, representing values of X!", type = 'error')
+      return(NULL)
+    }
+    
+    req(input$exercise_3_page_upload_values_y)
+    file.y <- input$exercise_3_page_upload_values_y
+    if (!all(file_ext(file.y$name) == 'csv')) {
+      showNotification("Y values: You must choose a csv file!", type = 'error')
+      return(NULL)
+    }
+    
+    y.csv.data <- read.csv(file.y$datapath)
+    if (!ncol(y.csv.data) == 1) {
+      showNotification("Y values: file must contain only 1 column, representing values of Y!", type = 'error')
+      return(NULL)
+    }
+    
+    req(input$exercise_3_page_upload_probs)
+    file.probs <- input$exercise_3_page_upload_probs
+    if (!all(file_ext(file.probs$name) == 'csv')) {
+      showNotification("Probabilities: You must choose a csv file!", type = 'error')
+      return(NULL)
+    }
+    
+    probs.csv.data <- read.csv(file.probs$datapath)
+    if (!ncol(probs.csv.data) == 1) {
+      showNotification("Probabilities: file must contain only 1 column, representing probabilities!", type = 'error')
+      return(NULL)
+    }
+    
+    if (!sum(probs.csv.data[,1]) == 1) {
+      showNotification("Sum of probabilities must equal 1!", type = 'error')
+      return(NULL)
+    }
+    return(data.frame(x.csv.data[,1], y.csv.data[,1], probs.csv.data[,1]))
+  })
+  observeEvent(GetInputData(), {
+    show('exercise_3_page_run_button')})
+  observeEvent(input$exercise_3_page_run_button, {
+    data <- GetInputData()
+    input.outcomes.x <- unique(data[,1]) 
+    ## unique for removing elements recycled by data.frame() to match dimension of probs vector
+    input.outcomes.y <- unique(data[,2])
+    input.probs <- data[,3]
+    
+    joint.input <- jointRVFromInput(input.outcomes.x, input.outcomes.y, input.probs)
+    marginal(joint.input, 1)
+    marginal(joint.input, 2)
+    x <- marginal(joint.input,1)
+    y <- marginal(joint.input,2)
+    MedieX <- E(x)
+    MedieY <- E(y)
+    VarX <- V(x)
+    VarY <- V(y)
+    DispX <- sqrt(VarX)
+    DispY <- sqrt(VarY)
+    Cov <- E(MultiplyRV((x-E(x)),(y-E(y))))
+    Cor <- Cov/(DispX * DispY)
+    output$exercise_3_page_output_x_average <- renderText({paste("Medie X: ", MedieX)})
+    output$exercise_3_page_output_y_average <- renderText({paste("Medie Y: ", MedieY)})
+    output$exercise_3_page_output_x_dispersion <- renderText({paste("Dispersie X: ", DispX)})
+    output$exercise_3_page_output_y_dispersion <- renderText({paste("Dispersie Y: ", DispY)})
+    output$exercise_3_page_output_x_y_covariance <- renderText({paste("Covarianta: ", Cov)})
+    output$exercise_3_page_output_x_y_correlation_coefficient <- renderText({paste("Coeficient de relatie: ", Cor)})
+    show('exercise_3_page_output_x_average')
+    show('exercise_3_page_output_y_average')
+    show('exercise_3_page_output_x_dispersion')
+    show('exercise_3_page_output_y_dispersion')
+    show('exercise_3_page_output_x_y_covariance')
+    show('exercise_3_page_output_x_y_correlation_coefficient')
+    
+    output$exercise_3_page_output_x <- renderPlot({plot(x, main="X")})
+    output$exercise_3_page_output_y <- renderPlot({plot(y, main="Y")})
+    show('exercise_3_page_output_x')
+    show('exercise_3_page_output_y')
+  })
 }
--- src/ui/exercise_3_page_content.R
+++ src/ui/exercise_3_page_content.R
@@ -2,101 +2,43 @@ Exercise3PageContent <- function() {
   fluid.page <- fluidPage(
     useShinyjs(),
     h1("Exercise 3", align = 'center'),
-    
-    # ## TODO: idea:
-    # inputs:
-    #   distribution name
-    #   number of parameters:
-    #     For each: name
-    #   type
-    #   description
-    #   formulas/values for:
-    #     mean
-    #     variance
-    #     median
-    #     mode
-    #     skewness
-    #     excess_kurtosis
-    # add distribution to list
-    # 
-    # output:
-    #   select distribution from list -> add each parameter -> same output as ex 2
-    #
-    # To transform input text to function: https://stackoverflow.com/questions/15260245/r-convert-text-field-to-function
-  
-    textInput(
-      'exercise_3_page_distribution_name',
-      label = 'Distribution name:',
-      placeholder = 'Name'
+    h2("Analysis of joint distribution", align = 'center'),
+    fileInput(
+      'exercise_3_page_upload_values_x',
+      label = 'Upload CSV file for values of discrete random variable X',
+      multiple = FALSE,
+      accept = '.csv',
+      buttonLabel = "File upload"
     ),
-    numericInput(
-      'exercise_3_page_nr_parameters',
-      label = 'Number of distribution parameters',
-      value=1
+    fileInput(
+      'exercise_3_page_upload_values_y',
+      label = 'Upload CSV file for values of discrete random variable Y',
+      multiple = FALSE,
+      accept = '.csv',
+      buttonLabel = "File upload"
     ),
-    ## TODO: nr of parameter names to input should depend on parameter number!
-    textInput(
-      'exercise_3_page_parameter_name',
-      label = 'Parameter name',
-      placeholder = 'Parameter'
+    fileInput(
+      'exercise_3_page_upload_probs',
+      label = 'Upload CSV file for probabilities of joint random variable XandY',
+      multiple = FALSE,
+      accept = '.csv',
+      buttonLabel = "File upload"
     ),
-    checkboxInput(
-      'exercise_3_page_is_continuous',
-      label = 'Distribution type (0 - discrete, 1 - continuous)',
-      value = FALSE
+    hidden(
+      actionButton(
+        'exercise_3_page_run_button',
+        label = 'Run',
+        icon = icon('play')
+      ),
+      plotOutput('exercise_3_page_output_x'),
+      plotOutput('exercise_3_page_output_y'),
+      textOutput('exercise_3_page_output_x_average'),
+      textOutput('exercise_3_page_output_y_average'),
+      textOutput('exercise_3_page_output_x_dispersion'),
+      textOutput('exercise_3_page_output_y_dispersion'),
+      textOutput('exercise_3_page_output_x_y_covariance'),
+      textOutput('exercise_3_page_output_x_y_correlation_coefficient')
     ),
-    textInput(
-      'exercise_3_page_description',
-      label = 'Distribution description',
-      placeholder = 'Description'
-    ),
-    textInput(
-      'exercise_3_page_mean',
-      label = 'Distribution mean',
-      placeholder = '0'
-    ),
-    textInput(
-      'exercise_3_page_variance',
-      label = 'Distribution variance',
-      placeholder = '0'
-    ),
-    textInput(
-      'exercise_3_page_median',
-      label = 'Distribution median',
-      placeholder = '0'
-    ),
-    textInput(
-      'exercise_3_page_mode',
-      label = 'Distribution mode',
-      placeholder = '0'
-    ),
-    textInput(
-      'exercise_3_page_skewness',
-      label = 'Distribution skewness',
-      placeholder = '0'
-    ),
-    textInput(
-      'exercise_3_page_kurtosis',
-      label = 'Distribution excess kurtosis',
-      placeholder = '0'
-    ),
-    textInput(
-      'exercise_3_page_PDF',
-      label = 'Input PDF:',
-      placeholder = 'x ** 2'
-    ),
-    textInput(
-      'exercise_3_page_CDF',
-      label = 'Input CDF:',
-      placeholder = 'x ** 2'
-    ),
-    actionButton('exercise_3_page_action_button',
-                 'Add distribution',
-                 icon = icon('play')) %>%
-      hidden(),
   )
-  ## TODO: Add output
-      
-      
-  return (fluid.page)
+  return(fluid.page)
 }
